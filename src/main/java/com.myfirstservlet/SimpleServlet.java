f1a5850e310574a0f9e6b98b1c935cdf6f6893b3
Bibhav Singh <singhbibhav084@gmail.com>
2020-12-12 19:09:06 +0530

    Servlet Demo

---
+++ src/main/java/com.myfirstservlet/SimpleServlet.java
@@ -0,0 +1,21 @@
+package com.myfirstservlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class SimpleServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.println("<h1>Good Bye World from Bibhav</h1>");
+        printWriter.close();
+    }
+}
